836fc8321e1eb04f0f8fbda1dda62fdb714ab19d
Alisson Youssf <youssfbr@gmail.com>
2024-11-03 17:40:52 -0300

    Projeto criado

---
+++ src/test/java/com/github/youssfbr/tabelafipe/TabelafipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.tabelafipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelafipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
