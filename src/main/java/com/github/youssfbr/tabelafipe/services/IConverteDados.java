28d43899f289e73dc154500c9f66f2782f3e814a
Alisson Youssf <youssfbr@gmail.com>
2024-11-07 12:07:21 -0300

    Interface List

--- src/main/java/com/github/youssfbr/tabelafipe/services/IConverteDados.java
+++ src/main/java/com/github/youssfbr/tabelafipe/services/IConverteDados.java
@@ -1,5 +1,8 @@
 package com.github.youssfbr.tabelafipe.services;
 
+import java.util.List;
+
 public interface IConverteDados {
     <T> T obterDados(String json , Class<T> classe);
+    <T> List<T> obterLista(String json , Class<T> classe);
 }
