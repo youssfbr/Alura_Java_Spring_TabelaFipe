4ca124838640729601958ba82a686ff85f0c6c6a
Alisson Youssf <youssfbr@gmail.com>
2024-11-07 12:08:20 -0300

    Mensagem Opcoes

---
+++ src/main/java/com/github/youssfbr/tabelafipe/utils/Mensagens.java
@@ -0,0 +1,15 @@
+package com.github.youssfbr.tabelafipe.utils;
+
+public class Mensagens {
+
+    public static final String MENSAGENS_OPCOES =
+        """
+                
+                **** OPÇÕES ****
+                Carro
+                Moto
+                Caminhão
+
+                Digite uma das opções para consultar valores:
+                """;
+}
