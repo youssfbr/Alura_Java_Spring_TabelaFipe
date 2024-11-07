56b12b5e168f8f72bdadd5749ad1763cec2716e3
Alisson Youssf <youssfbr@gmail.com>
2024-11-07 12:05:37 -0300

    ConverteDados List

--- src/main/java/com/github/youssfbr/tabelafipe/services/ConverteDados.java
+++ src/main/java/com/github/youssfbr/tabelafipe/services/ConverteDados.java
@@ -2,7 +2,10 @@ package com.github.youssfbr.tabelafipe.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ConverteDados implements IConverteDados {
@@ -13,6 +16,20 @@ public class ConverteDados implements IConverteDados {
     public <T> T obterDados(String json , Class<T> classe) {
         try {
             return mapper.readValue(json , classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Outra maneira de obter lista
+    @Override
+    public <T> List<T> obterLista(String json , Class<T> classe) {
+
+        final CollectionType lista = mapper.getTypeFactory()
+                .constructCollectionType(List.class , classe);
+
+        try {
+            return mapper.readValue(json , lista);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
